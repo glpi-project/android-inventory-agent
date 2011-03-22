66993f76c3363ef750dc92a9b31c1679073cc737
Kevin Roy <kiniou@gmail.com>
2011-03-22 21:41:17 +0100

    [utils/string_format]
    * Added a reverse join function
    * Added an Integer to IPv4 function (no IPv6 support ... sorry :P)

--- src/org/fusionInventory/utils/StringUtils.java
+++ src/org/fusionInventory/utils/StringUtils.java
@@ -2,6 +2,7 @@ package org.fusionInventory.utils;
 
 import java.util.Collection;
 import java.util.Iterator;
+import java.util.Stack;
 
 public class StringUtils {
 
@@ -9,15 +10,44 @@ public class StringUtils {
         // Utility class.
     }
 
-    public static String join(Collection<String> collection, String delimiter) {
+    public static String join(Collection<String> collection, String delimiter, boolean reversed) {
         StringBuffer buffer = new StringBuffer();
         Iterator<String> iter = collection.iterator();
         while (iter.hasNext()) {
-            buffer.append(iter.next());
-            if (iter.hasNext()) {
-                buffer.append(delimiter);
+            if (!reversed) {
+                buffer.append(iter.next());
+                if (iter.hasNext()) {
+                    buffer.append(delimiter);
+
+                }
+            } else {
+                buffer.insert(0,iter.next());
+                if (iter.hasNext()) {
+                    buffer.insert(0,delimiter);
+
+                }
             }
         }
         return buffer.toString();
+    }
+
+    public static String join(Collection<String> collection, String delimiter) {
+        
+        return StringUtils.join(collection,delimiter,false);
+    }
+
+    public static byte[] int_to_byte(int value) {
+        return new byte[] { (byte) (value >>> 24), (byte) (value >>> 16), (byte) (value >>> 8), (byte) value };
+    }
+
+    public static String int_to_ip(int value) {
+        StringBuilder s = new StringBuilder();
+        byte[] b = int_to_byte(value);
+        Stack<String> stack = new Stack<String>();
+        for (byte c : b) {
+            stack.push(String.valueOf(0xFF & c));
+        }
+
+        return (StringUtils.join(stack, ".",true));
     }
 }
