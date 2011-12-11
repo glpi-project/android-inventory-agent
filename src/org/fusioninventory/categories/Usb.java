708de36e9e8e609b91378fdd1a352426add19289
Walid <walid.nouh@gmail.com>
2011-12-11 22:06:26 +0100

    Add french translation Do not peform usB inventory is SDK level < 12

--- src/org/fusioninventory/categories/Usb.java
+++ src/org/fusioninventory/categories/Usb.java
@@ -8,6 +8,7 @@ import org.fusioninventory.FusionInventory;
 import android.content.Context;
 import android.hardware.usb.UsbDevice;
 import android.hardware.usb.UsbManager;
+import android.os.Build;
 import android.util.Log;
 
 public class Usb
@@ -21,19 +22,23 @@ public class Usb
     public Usb(Context xCtx) {
         super(xCtx);
         
-        UsbManager manager = (UsbManager) xCtx.getSystemService(Context.USB_SERVICE);
-        HashMap<String, UsbDevice> devices = manager.getDeviceList();
-        Iterator<String> iterator = devices.keySet().iterator();
-        while (iterator.hasNext()) {
-        	String key = (String) iterator.next();
-        	FusionInventory.log(this,key, Log.VERBOSE);
-        	UsbDevice mydevice = devices.get(key);
-        	Category c = new Category(mCtx, "USBDEVICES");
-        	c.put("CLASS", Integer.toString(mydevice.getDeviceClass()));
-        	c.put("PRODUCTID", Integer.toString(mydevice.getProductId()));
-        	c.put("VENDORID", Integer.toString(mydevice.getVendorId()));
-        	c.put("SUBCLASS", Integer.toString(mydevice.getDeviceSubclass()));
-        	this.add(c);
+        //USB inventory comes with SDK level 12 !
+        if(Build.VERSION.SDK_INT > 12) {
+	
+	        UsbManager manager = (UsbManager) xCtx.getSystemService(Context.USB_SERVICE);
+	        HashMap<String, UsbDevice> devices = manager.getDeviceList();
+	        Iterator<String> iterator = devices.keySet().iterator();
+	        while (iterator.hasNext()) {
+	        	String key = (String) iterator.next();
+	        	FusionInventory.log(this,key, Log.VERBOSE);
+	        	UsbDevice mydevice = devices.get(key);
+	        	Category c = new Category(mCtx, "USBDEVICES");
+	        	c.put("CLASS", Integer.toString(mydevice.getDeviceClass()));
+	        	c.put("PRODUCTID", Integer.toString(mydevice.getProductId()));
+	        	c.put("VENDORID", Integer.toString(mydevice.getVendorId()));
+	        	c.put("SUBCLASS", Integer.toString(mydevice.getDeviceSubclass()));
+	        	this.add(c);
+	        }
         }
     }
 }
