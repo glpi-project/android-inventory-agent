83f0afd2a6d1bae32b7e8b4fd1030ba7d333e81f
Rafa Hernández <rhernandez@teclib.com>
2017-07-17 11:44:30 -0400

    feat(assets): add graphic resources and translations  (#16)
    * add new produc icon
    * add new splash screen
    * add fastlane deployment
    * add spanish translations
    
    Closes: https://github.com/flyve-mdm/flyve-mdm-android-inventory-agent/issues/13, https://github.com/flyve-mdm/flyve-mdm-android-inventory-agent/issues/14 and https://github.com/flyve-mdm/flyve-mdm-android-inventory-agent/issues/11

---
+++ ci/deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [[ "$TRAVIS_BRANCH" == "develop" && "$TRAVIS_PULL_REQUEST" == "false" ]];
+then
+    cd ci
+    tar -zxvf google.tar.gz
+    cd ..
+    fastlane android alpha storepass:'$KEYSTORE' keypass:'$ALIAS'
+fi
