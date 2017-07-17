884b32e170496c9fe69a88dc6cff5ee819d20c31
Rafa Hernández <rhernandez@teclib.com>
2017-07-17 11:12:12 -0400

    remove local branch on deploy script

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-if [[ "$TRAVIS_BRANCH" == "feature-gplay" ]];
-then
-    cd ci
-    tar -zxvf google.tar.gz
-    cd ..
-    fastlane android alpha storepass:'$KEYSTORE' keypass:'$ALIAS'
-fi
-
 if [[ "$TRAVIS_BRANCH" == "develop" && "$TRAVIS_PULL_REQUEST" == "false" ]];
 then
     cd ci
