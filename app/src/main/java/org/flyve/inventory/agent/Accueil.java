a9a47848f8a6c0b0717584766e5f83f7899693f9
Rafa Hernandez <rhernandez@teclib.com>
2017-09-01 12:05:40 -0400

    refactor(accueil): refactor code

--- app/src/main/java/org/flyve/inventory/agent/Accueil.java
+++ app/src/main/java/org/flyve/inventory/agent/Accueil.java
@@ -1,3 +1,28 @@
+/*
+ * Copyright (C) 2017 Teclib'
+ *
+ * This file is part of Flyve MDM Inventory Agent Android.
+ *
+ * Flyve MDM Inventory Agent Android is a subproject of Flyve MDM. Flyve MDM is a mobile
+ * device management software.
+ *
+ * Flyve MDM Android is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * Flyve MDM Inventory Agent Android is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * ------------------------------------------------------------------------------
+ * @author    Rafael Hernandez - rafaelje
+ * @copyright Copyright (c) 2017 Flyve MDM
+ * @license   GPLv3 https://www.gnu.org/licenses/gpl-3.0.html
+ * @link      https://github.com/flyvemdm/flyve-mdm-android-inventory-agent
+ * @link      http://www.glpi-project.org/
+ * ------------------------------------------------------------------------------
+ */
 package org.flyve.inventory.agent;
 
 import android.content.ComponentName;
@@ -22,254 +47,17 @@ import android.preference.PreferenceManager;
 import android.util.Log;
 import android.widget.Toast;
 
-public class Accueil extends PreferenceActivity implements
-OnSharedPreferenceChangeListener {
+import org.flyve.inventory.agent.utils.FlyveLog;
+
+public class Accueil extends PreferenceActivity implements OnSharedPreferenceChangeListener {
 
     private Messenger mAgentService = null;
-
-    private String[] STATUS_AGENT = null;
+    private  String[] statusAgent = null;
     private boolean isAgentOk = false;
     private String barcode = null;
-
     private boolean notif = false;
-
-    private boolean ssh = false;
-
     private SharedPreferences customSharedPreference;
-
-    private static final String TAG = "DroidSSHd";
-    final Handler mHandler = new Handler();
-    private long mUpdateUIdelay = 500L;
-    private Intent mDropbearDaemonHandlerService;
-
-    public Messenger getmAgentService(){
-        return mAgentService;
-    }
-
-    public static void log(Object obj, String msg, int level) {
-        String final_msg = String.format("[%s] %s", obj.getClass().getName(), msg);
-        Log.println(level, "FusionInventory", final_msg);
-    }
-
-    class IncomingHandler
-            extends Handler {
-            @Override
-                public void handleMessage(Message msg) {
-                    // TODO Auto-generated method stub
-                    Accueil.log(this, " message received " + msg.toString(), Log.INFO);
-
-                    switch (msg.what) {
-
-                        case Agent.MSG_AGENT_STATUS:
-
-                            Accueil.log(this, STATUS_AGENT[msg.arg1], Log.INFO);
-                            isAgentOk = (msg.arg1 == 0 ? true : false);
-                            break;
-                        case Agent.MSG_INVENTORY_FINISHED:
-
-                            try {
-                                mAgentService.send(Message.obtain(null, Agent.MSG_INVENTORY_RESULT));
-                            } catch (RemoteException e) {
-                                // TODO Auto-generated catch block
-                                e.printStackTrace();
-                            }
-                            break;
-
-                        case Agent.MSG_INVENTORY_RESULT:
-                            Bundle bXML = msg.peekData();
-                            if (bXML != null) {
-                                try {
-                                    mAgentService.send(Message.obtain(null, Agent.MSG_AGENT_STATUS));
-                                } catch (RemoteException e) {
-                                    // TODO Auto-generated catch block
-                                    e.printStackTrace();
-                                }
-                            }
-                            break;
-
-                        default:
-                            super.handleMessage(msg);
-                    }
-                }
-    }
-
-    final Messenger mMessenger = new Messenger(new IncomingHandler());
-
-    private ServiceConnection mConnection = new ServiceConnection() {
-
-        @Override
-            public void onServiceConnected(ComponentName name, IBinder service) {
-
-                mAgentService = new Messenger(service);
-
-                try {
-                    Message msg = Message.obtain();
-                    msg.replyTo = mMessenger;
-                    msg.what = Agent.MSG_CLIENT_REGISTER;
-                    mAgentService.send(msg);
-
-                    mAgentService.send(Message.obtain(null, Agent.MSG_AGENT_STATUS));
-                } catch (RemoteException e) {
-                    // TODO Auto-generated catch block
-                    e.printStackTrace();
-                }
-
-                if (notif){
-                    Toast.makeText(Accueil.this, R.string.agent_connected, Toast.LENGTH_SHORT).show();
-                }
-            }
-
-        @Override
-            public void onServiceDisconnected(ComponentName name) {
-                isAgentOk = false;
-                mAgentService = null;
-
-                if (notif){
-                    Toast.makeText(Accueil.this, R.string.agent_disconnected, Toast.LENGTH_SHORT).show();
-                }
-            }
-    };
-
-    private boolean mIsBound = false;
-
-    void doBindService() {
-        // Establish a connection with the service. We use an explicit
-        // class name because we want a specific service implementation that
-        // we know will be running in our own process (and thus won't be
-        // supporting component replacement by other applications).
-
-        // mIsBound = bindService(new Intent(FusionInventory.this, Agent.class),
-        // mConnection, Context.BIND_AUTO_CREATE);
-
-        ComponentName result = startService(new Intent("org.fusioninventory.Agent"));
-        if (result != null) {
-            Accueil.log(this, " Agent started ", Log.INFO);
-        } else {
-            Accueil.log(this, " Agent already started ", Log.ERROR);
-        }
-
-        mIsBound = bindService(new Intent(Accueil.this, Agent.class), mConnection, Context.BIND_NOT_FOREGROUND);
-
-        if (mIsBound) {
-            Accueil.log(this, "Connected sucessfully to Agent service", Log.INFO);
-        } else {
-            Accueil.log(this, "Failed to connect to Agent service", Log.ERROR);
-        }
-
-    }
-
-    void doUnbindService() {
-        if (mIsBound) {
-            // Detach our existing connection.
-            unbindService(mConnection);
-            mIsBound = false;
-        }
-    }
-
-
-    @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            STATUS_AGENT = getResources().getStringArray(R.array.agent_status);
-
-            addPreferencesFromResource(R.xml.accueil);
-
-            doBindService();
-
-            customSharedPreference = PreferenceManager.getDefaultSharedPreferences(this);
-            notif = customSharedPreference.getBoolean("notif", false);
-
-            Preference autoStartInventory = findPreference("autoStartInventory");
-            autoStartInventory.setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
-                public boolean onPreferenceChange(Preference arg0,
-                    Object arg1) {
-                    // TODO Auto-generated method stub
-
-                    notif = customSharedPreference.getBoolean("notif", false);
-
-                    if (notif){
-                        Toast.makeText(getBaseContext(), R.string.agent_reboot, Toast.LENGTH_SHORT).show();
-                    }
-
-                    stopService(new Intent("org.fusioninventory.Agent"));
-                    startService(new Intent("org.fusioninventory.Agent"));
-
-                    return true;
-                }
-
-            });
-
-            Preference timeInventory = findPreference("timeInventory");
-            timeInventory.setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
-                public boolean onPreferenceChange(Preference arg0,
-                    Object arg1) {
-                    // TODO Auto-generated method stub
-
-                    notif = customSharedPreference.getBoolean("notif", false);
-
-                    if (notif){
-                        Toast.makeText(getBaseContext(), R.string.agent_reboot,Toast.LENGTH_SHORT).show();
-                    }
-
-                    stopService(new Intent("org.fusioninventory.Agent"));
-                    startService(new Intent("org.fusioninventory.Agent"));
-
-                    return true;
-                }
-
-            });
-
-            Preference runInventory = findPreference("runInventory");
-            runInventory.setOnPreferenceClickListener(new OnPreferenceClickListener() {
-                public boolean onPreferenceClick(Preference preference) {
-                    Message msg;
-
-                    if (isAgentOk) {
-                        try {
-
-                            Toast.makeText(Accueil.this, R.string.inventory_started, Toast.LENGTH_LONG).show();
-
-                            isAgentOk = false;
-
-                            msg = Message.obtain(null, Agent.MSG_INVENTORY_START);
-                            msg.replyTo = mMessenger;
-                            if (barcode != null) {
-                                Bundle b = new Bundle();
-                                b.putString("BARCODE", barcode);
-                                msg.setData(b);
-                            }
-                            mAgentService.send(msg);
-
-
-                        } catch (RemoteException e) {
-                            // TODO Auto-generated catch block
-                            e.printStackTrace();
-                        }
-
-                        isAgentOk = true;
-                    }
-
-                    if (isAgentOk) {
-
-                        isAgentOk = false;
-
-                        msg = Message.obtain(null, Agent.MSG_INVENTORY_SEND);
-                        msg.replyTo = mMessenger;
-                        try {
-                            mAgentService.send(msg);
-                        } catch (RemoteException e) {
-                            // TODO Auto-generated catch block
-                            e.printStackTrace();
-                        }
-
-                        isAgentOk = true;
-                    }
-
-                    return true;
-                }
-
-            });
-        }
+    private final Messenger mMessenger = new Messenger(new IncomingHandler());
 
     protected void onResume() {
         super.onResume();
@@ -281,6 +69,158 @@ OnSharedPreferenceChangeListener {
         getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener( this );
     }
 
+    private ServiceConnection mConnection = new ServiceConnection() {
+
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+
+            mAgentService = new Messenger(service);
+
+            try {
+                Message msg = Message.obtain();
+                msg.replyTo = mMessenger;
+                msg.what = Agent.MSG_CLIENT_REGISTER;
+                mAgentService.send(msg);
+
+                mAgentService.send(Message.obtain(null, Agent.MSG_AGENT_STATUS));
+            } catch (RemoteException e) {
+                FlyveLog.e(e.getMessage());
+            }
+
+            if (notif){
+                Toast.makeText(Accueil.this, R.string.agent_connected, Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            isAgentOk = false;
+            mAgentService = null;
+
+            if (notif){
+                Toast.makeText(Accueil.this, R.string.agent_disconnected, Toast.LENGTH_SHORT).show();
+            }
+        }
+    };
+
+    void doBindService() {
+        // Establish a connection with the service. We use an explicit
+        // class name because we want a specific service implementation that
+        // we know will be running in our own process (and thus won't be
+        // supporting component replacement by other applications).
+
+        ComponentName result = startService(new Intent("org.flyve.inventory.agent"));
+        if (result != null) {
+            FlyveLog.log(this, " Agent started ", Log.INFO);
+        } else {
+            FlyveLog.log(this, " Agent already started ", Log.ERROR);
+        }
+
+        boolean mIsBound = bindService(new Intent(Accueil.this, Agent.class), mConnection, Context.BIND_NOT_FOREGROUND);
+
+        if (mIsBound) {
+            FlyveLog.log(this, "Connected successfully to Agent service", Log.INFO);
+        } else {
+            FlyveLog.log(this, "Failed to connect to Agent service", Log.ERROR);
+        }
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        statusAgent = getResources().getStringArray(R.array.agent_status);
+
+        addPreferencesFromResource(R.xml.accueil);
+
+        doBindService();
+
+        customSharedPreference = PreferenceManager.getDefaultSharedPreferences(this);
+        notif = customSharedPreference.getBoolean("notif", false);
+
+        Preference autoStartInventory = findPreference("autoStartInventory");
+        autoStartInventory.setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
+            public boolean onPreferenceChange(Preference arg0, Object arg1) {
+
+                notif = customSharedPreference.getBoolean("notif", false);
+
+                if (notif){
+                    Toast.makeText(getBaseContext(), R.string.agent_reboot, Toast.LENGTH_SHORT).show();
+                }
+
+                stopService(new Intent("org.flyve.inventory.agent"));
+                startService(new Intent("org.flyve.inventory.agent"));
+
+                return true;
+            }
+
+        });
+
+        Preference timeInventory = findPreference("timeInventory");
+        timeInventory.setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
+            public boolean onPreferenceChange(Preference arg0, Object arg1) {
+
+                notif = customSharedPreference.getBoolean("notif", false);
+
+                if (notif){
+                    Toast.makeText(getBaseContext(), R.string.agent_reboot,Toast.LENGTH_SHORT).show();
+                }
+
+                stopService(new Intent("org.flyve.inventory.agent"));
+                startService(new Intent("org.flyve.inventory.agent"));
+
+                return true;
+            }
+
+        });
+
+        Preference runInventory = findPreference("runInventory");
+        runInventory.setOnPreferenceClickListener(new OnPreferenceClickListener() {
+            public boolean onPreferenceClick(Preference preference) {
+                Message msg;
+
+                if (isAgentOk) {
+                    try {
+                        Toast.makeText(Accueil.this, R.string.inventory_started, Toast.LENGTH_LONG).show();
+
+                        isAgentOk = false;
+
+                        msg = Message.obtain(null, Agent.MSG_INVENTORY_START);
+                        msg.replyTo = mMessenger;
+                        if (barcode != null) {
+                            Bundle b = new Bundle();
+                            b.putString("BARCODE", barcode);
+                            msg.setData(b);
+                        }
+                        mAgentService.send(msg);
+                    } catch (RemoteException e) {
+                        FlyveLog.e(e.getMessage());
+                    }
+
+                    isAgentOk = true;
+                }
+
+                if (isAgentOk) {
+
+                    isAgentOk = false;
+
+                    msg = Message.obtain(null, Agent.MSG_INVENTORY_SEND);
+                    msg.replyTo = mMessenger;
+                    try {
+                        mAgentService.send(msg);
+                    } catch (RemoteException e) {
+                        FlyveLog.e(e.getMessage());
+                    }
+
+                    isAgentOk = true;
+                }
+
+                return true;
+            }
+
+        });
+    }
+
     public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
         Preference pref = findPreference(key);
         if (pref instanceof EditTextPreference) {
@@ -290,6 +230,41 @@ OnSharedPreferenceChangeListener {
         if (pref instanceof ListPreference) {
             ListPreference listp = (ListPreference) pref;
             pref.setSummary(listp.getValue());
+        }
+    }
+
+    private class IncomingHandler extends Handler {
+        @Override
+        public void handleMessage(Message msg) {
+            FlyveLog.log(this, " message received " + msg.toString(), Log.INFO);
+
+            switch (msg.what) {
+                case Agent.MSG_AGENT_STATUS:
+
+                    FlyveLog.log(this, statusAgent[msg.arg1], Log.INFO);
+                    isAgentOk = msg.arg1 == 0;
+                    break;
+                case Agent.MSG_INVENTORY_FINISHED:
+                    try {
+                        mAgentService.send(Message.obtain(null, Agent.MSG_INVENTORY_RESULT));
+                    } catch (RemoteException e) {
+                        FlyveLog.e(e.getMessage());
+                    }
+                    break;
+
+                case Agent.MSG_INVENTORY_RESULT:
+                    Bundle bXML = msg.peekData();
+                    if (bXML != null) {
+                        try {
+                            mAgentService.send(Message.obtain(null, Agent.MSG_AGENT_STATUS));
+                        } catch (RemoteException e) {
+                            FlyveLog.e(e.getMessage());
+                        }
+                    }
+                    break;
+                default:
+                    super.handleMessage(msg);
+            }
         }
     }
 }
