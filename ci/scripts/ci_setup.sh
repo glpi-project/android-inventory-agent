a2167fefa9ca075ee500982b221f6b56669d67b7
Rafa Hernandez <rhernandez@teclib.com>
2017-09-08 17:47:13 -0400

    ci(circleci): install tools

--- ci/scripts/ci_setup.sh
+++ ci/scripts/ci_setup.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
 
-# accepting licenses - creating a folder to store the license CRC
-mkdir -p ${ANDROID_HOME}/licenses || true
-rm ${ANDROID_HOME}/licenses/* || true
+# install fastlane
+gem install fastlane
 
-echo -e "sdk.dir=${ANDROID_HOME}\nndk.dir=${ANDROID_NDK}" > local.properties
+# install globally
+npm install -g conventional-github-releaser
 
-#this value was taken from my local machine, after I accepted it locally.
-echo -e "8933bad161af4178b1185d1a37fbf41ea5269c55\c" > ${ANDROID_HOME}/licenses/android-sdk-license
-echo -e "79120722343a6f314e0719f863036c702b0e6b2a\n84831b9409646a918e30573bab4c9c91346d8abd\c" > ${ANDROID_HOME}/licenses/android-sdk-preview-license
-echo -e "8403addf88ab4874007e1c1e80a0025bf2550a37\c" > ${ANDROID_HOME}/licenses/intel-android-sysimage-license
+# install node package available on package.json
+npm install
 
-# workaround for plugin error https://code.google.com/p/android/issues/detail?id=212309
-./gradlew --no-daemon --stacktrace dependencies || true
-./gradlew --no-daemon --stacktrace clean
+# install transifex CLI
+pip install virtualenv
+virtualenv ~/env
+source ~/env/bin/activate
+pip install transifex-client
+sudo echo $'[https://www.transifex.com]\nhostname = https://www.transifex.com\nusername = '"$TRANSIFEX_USER"$'\npassword = '"$TRANSIFEX_API_TOKEN"$'\ntoken = '"$TRANSIFEX_API_TOKEN"$'\n' > ~/.transifexrc
