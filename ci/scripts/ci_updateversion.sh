129266e39c16568848c84bd577066c060e0373ba
Rafa Hernandez <rhernandez@teclib.com>
2017-09-09 13:12:14 -0400

    ci(circleci): update version and code

---
+++ ci/scripts/ci_updateversion.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# go to folder
+cd flyve
+
+# increment version code, need to be unique to send to store
+gradle updateVersionCode -P vCode=$CIRCLE_BUILD_NUM
+
+# increment version on package.json, create tag and commit with changelog
+npm run release -- -m "ci(release): generate **CHANGELOG.md** for version %s"
+
+# Get version number from package.json
+export GIT_TAG=$(jq -r ".version" package.json)
+
+# update version name generate on package json
+gradle updateVersionName -P vName=$GIT_TAG
