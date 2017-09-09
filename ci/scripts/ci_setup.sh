4da3307161fed8e361cd1a1177b0540842b13557
Rafa Hernandez <rhernandez@teclib.com>
2017-09-08 20:41:28 -0400

    ci(circleci): install component

--- ci/scripts/ci_setup.sh
+++ ci/scripts/ci_setup.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 
+# install gems
+sudo apt-get install rubygems-integration
+
 # install fastlane
 gem install fastlane
+
+# install Node.js v7
+curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
+sudo apt-get install -y nodejs
 
 # install globally
 npm install -g conventional-github-releaser
@@ -10,7 +17,6 @@ npm install -g conventional-github-releaser
 npm install
 
 # install transifex CLI
-python3 -m venv venv
-. venv/bin/activate
+apt-get -y install python-pip
 pip install transifex-client
 sudo echo $'[https://www.transifex.com]\nhostname = https://www.transifex.com\nusername = '"$TRANSIFEX_USER"$'\npassword = '"$TRANSIFEX_API_TOKEN"$'\ntoken = '"$TRANSIFEX_API_TOKEN"$'\n' > ~/.transifexrc
