265f898804ba5503885ea34e882e8ce745b3b3e8
Rafa Hernandez <rhernandez@teclib.com>
2017-09-09 16:01:30 -0400

    ci(circleci): add start emulator script

---
+++ ci/scripts/start_emulator.sh
@@ -0,0 +1,7 @@
+DEFAULT_API=23
+
+# create package
+sdkmanager "system-images;android-$DEFAULT_API;google_apis;x86"
+
+# Launch emulator
+echo no | avdmanager create avd -n test -k "system-images;android-$DEFAULT_API;google_apis;x86" -b x86 -c 100M -d 7 -f
