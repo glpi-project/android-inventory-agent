c0e1634480e96c3a51be0aff780d0dcc8d32004b
Rafa Hernandez <rhernandez@teclib.com>
2017-09-08 21:10:22 -0400

    ci(circleci): add fastlane install

--- ci/scripts/ci_setup.sh
+++ ci/scripts/ci_setup.sh
@@ -1,22 +1,25 @@
 #!/usr/bin/env bash
 
+# go to our folder
+cd flyve
+
 # install gems
-sudo apt-get install rubygems-integration
+sudo apt-get install ruby-full build-essential
 
 # install fastlane
 gem install fastlane
 
 # install Node.js v7
 curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
-sudo apt-get install -y nodejs
+sudo sudo apt-get install -y nodejs
 
 # install globally
 sudo npm install -g conventional-github-releaser
 
 # install node package available on package.json
-npm install ~/flyve
+npm install
 
 # install transifex CLI
-apt-get -y install python-pip
+sudo apt-get -y install python-pip
 pip install transifex-client
 sudo echo $'[https://www.transifex.com]\nhostname = https://www.transifex.com\nusername = '"$TRANSIFEX_USER"$'\npassword = '"$TRANSIFEX_API_TOKEN"$'\ntoken = '"$TRANSIFEX_API_TOKEN"$'\n' > ~/.transifexrc
