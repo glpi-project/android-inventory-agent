77e606f71469536919dfc756b6c07e93ad522f48
Rafa Hernandez <rhernandez@teclib.com>
2017-09-09 18:46:13 -0400

    ci(circleci): remove BUILD_TOOL from script add on site

--- ci/scripts/ci_setup.sh
+++ ci/scripts/ci_setup.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-# build tool
-export BUILD_TOOL=25.0.0
-
 # decrypt deploy on google play file
 openssl aes-256-cbc -d -out ci/gplay.json -in ci/gplay.json.enc -k $ENCRYPTED_KEY
 
