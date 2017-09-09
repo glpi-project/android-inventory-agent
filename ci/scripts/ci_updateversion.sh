1eb633fb307c0e96944c51eb743cc9a20f6c7962
Rafa Hernandez <rhernandez@teclib.com>
2017-09-09 13:48:34 -0400

    ci(circleci): refactor gradle to gradlew

--- ci/scripts/ci_updateversion.sh
+++ ci/scripts/ci_updateversion.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # increment version code, need to be unique to send to store
-gradle updateVersionCode -P vCode=$CIRCLE_BUILD_NUM
+./gradlew updateVersionCode -P vCode=$CIRCLE_BUILD_NUM
 
 # increment version on package.json, create tag and commit with changelog
 npm run release -- -m "ci(release): generate **CHANGELOG.md** for version %s"
@@ -10,4 +10,4 @@ npm run release -- -m "ci(release): generate **CHANGELOG.md** for version %s"
 export GIT_TAG=$(jq -r ".version" package.json)
 
 # update version name generate on package json
-gradle updateVersionName -P vName=$GIT_TAG
+./gradlew updateVersionName -P vName=$GIT_TAG
