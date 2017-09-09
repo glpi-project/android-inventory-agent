f96ba2e6358e834f7f68f1f8e7a260e3133a850c
Rafa Hernandez <rhernandez@teclib.com>
2017-09-09 13:58:53 -0400

    ci(circleci): setup git config

--- ci/scripts/ci_setup.sh
+++ ci/scripts/ci_setup.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 # install gems
-sudo apt-get install ruby-full build-essential
+# sudo apt-get install ruby-full build-essential
 
 # install fastlane
-sudo gem install fastlane
+# sudo gem install fastlane
 
 # install Node.js v7
 curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
@@ -20,3 +20,9 @@ sudo npm install
 sudo apt-get -y install python-pip
 sudo pip install transifex-client
 sudo echo $'[https://www.transifex.com]\nhostname = https://www.transifex.com\nusername = '"$TRANSIFEX_USER"$'\npassword = '"$TRANSIFEX_API_TOKEN"$'\ntoken = '"$TRANSIFEX_API_TOKEN"$'\n' > ~/.transifexrc
+
+# config git
+git config --global user.email $GH_EMAIL
+git config --global user.name "Flyve MDM"
+#git remote remove origin
+#git remote add origin https://$GH_USER:$GH_TOKEN@github.com/$TRAVIS_REPO_SLUG.git
