28064a63a9711c86fdb73ae7be2cfb0230b625f0
Rafa Hernandez <rhernandez@teclib.com>
2017-09-11 12:53:07 -0400

    ci(circleci): emulator start

--- ci/scripts/start_emulator.sh
+++ ci/scripts/start_emulator.sh
@@ -5,3 +5,4 @@ sdkmanager "system-images;android-$DEFAULT_API;google_apis;x86"
 
 # Launch emulator
 echo no | avdmanager create avd -n test -k "system-images;android-$DEFAULT_API;google_apis;x86" -b x86 -c 100M -d 7 -f
+
