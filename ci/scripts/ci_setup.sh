3d0aa32a5f223db45adecbd05ce41301f46fc84a
Rafa Hernandez <rhernandez@teclib.com>
2017-09-11 17:35:39 -0400

    ci(transifex): add transifex workflow

--- ci/scripts/ci_setup.sh
+++ ci/scripts/ci_setup.sh
@@ -24,11 +24,6 @@ sudo npm install -g conventional-github-releaser
 # install node package available on package.json
 sudo npm install
 
-# install transifex CLI
-sudo apt-get -y install python-pip
-sudo pip install transifex-client
-sudo echo $'[https://www.transifex.com]\nhostname = https://www.transifex.com\nusername = '"$TRANSIFEX_USER"$'\npassword = '"$TRANSIFEX_API_TOKEN"$'\ntoken = '"$TRANSIFEX_API_TOKEN"$'\n' > ~/.transifexrc
-
 # config git
 git config --global user.email $GH_EMAIL
 git config --global user.name "Flyve MDM"
