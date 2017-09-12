46f3bca89530bf8137840c64d54b8fad23eb3414
Rafa Hernandez <rhernandez@teclib.com>
2017-09-12 11:07:20 -0400

    ci(javadoc): get reports folder from branch

--- ci/scripts/ci_javadoc.sh
+++ ci/scripts/ci_javadoc.sh
@@ -3,16 +3,20 @@
 # Generate javadoc this folder must be on .gitignore
 javadoc -d ./reports$1/javadoc -sourcepath ./app/src/main/java -subpackages .
 
+# add reports
+git add reports$1 -f
+
+# create commit with temporary report folder
+git commit -m "tmp report commit"
+
 # get gh-pages branch
 git fetch origin gh-pages
 
 # move to gh-pages
 git checkout gh-pages
 
-sudo git clean -fdx
-
 # get javadoc folder
-git checkout $TRAVIS_BRANCH reports$1/javadoc
+git checkout $CIRCLE_BRANCH reports$1/javadoc
 
 # git add javadoc folder
 git add reports$1/javadoc
