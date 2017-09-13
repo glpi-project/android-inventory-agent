39a631ae96af0f0a51271e808d0c5d2f68695f8c
Rafa Hernandez <rhernandez@teclib.com>
2017-09-12 22:15:07 -0400

    ci(coverage): clean git files

--- ci/scripts/ci_coverage.sh
+++ ci/scripts/ci_coverage.sh
@@ -9,12 +9,12 @@ mv -v app/build/reports/coverage reports$1
 #move Android test
 mv -v app/build/reports/androidTests reports$1
 
-# # replace .resources with resource because github don't support folders with "_" or "." at the beginning
-# mv reports$1/coverage/debug/.resources reports$1/coverage/debug/resources
-#
-# index=$(<reports$1/coverage/debug/index.html)
-# newindex="${index//.resources/resources}"
-# echo $newindex > reports$1/coverage/debug/index.html
+# replace .resources with resource because github don't support folders with "_" or "." at the beginning
+mv reports$1/coverage/debug/.resources reports$1/coverage/debug/resources
+
+index=$(<reports$1/coverage/debug/index.html)
+newindex="${index//.resources/resources}"
+echo $newindex > reports$1/coverage/debug/index.html
 
 # add code coverage and test result
 git add reports$1 -f
@@ -27,6 +27,9 @@ git fetch origin gh-pages
 
 # move to gh-pages
 git checkout gh-pages
+
+# clean
+sudo git clean -fdx
 
 # get documentation folder
 git checkout $CIRCLE_BRANCH reports$1
