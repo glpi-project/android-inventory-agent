daf64ff0d12db40319ee5b50500a2d5fa8b3622d
Rafa Hernandez <rhernandez@teclib.com>
2017-09-12 22:30:20 -0400

    ci(coverage): replace folders with dot for design

--- ci/scripts/ci_coverage.sh
+++ ci/scripts/ci_coverage.sh
@@ -10,11 +10,11 @@ mv -v app/build/reports/coverage reports$1
 mv -v app/build/reports/androidTests reports$1
 
 # replace .resources with resource because github don't support folders with "_" or "." at the beginning
-mv reports$1/coverage/debug/.resources reports$1/coverage/debug/resources
+mv reports$1/debug/.resources reports$1/debug/resources
 
-index=$(<reports$1/coverage/debug/index.html)
+index=$(<reports$1/debug/index.html)
 newindex="${index//.resources/resources}"
-echo $newindex > reports$1/coverage/debug/index.html
+echo $newindex > reports$1/debug/index.html
 
 # add code coverage and test result
 git add reports$1 -f
