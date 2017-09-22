83259e740dd3f5627959eb734e5cf511db5690e0
Gianfranco Manganiello <gmanganiello@teclib.com>
2017-09-22 18:36:30 -0400

    ci(htmlproofer): change parameters and ignore conflicting files

--- ci/tets-jekyll.sh
+++ ci/tets-jekyll.sh
@@ -1,12 +1,6 @@
-# gem install jekyll bundler
-# bundle exec jekyll build
-# rm -rf _site/reports
-# ./node_modules/.bin/htmlhint _site
-
-
 #!/usr/bin/env bash
 set -e # halt script on error
 
 bundle exec jekyll build
 rm -rf _site/reports
-bundle exec htmlproofer ./_site --allow-hash-href true --assume-extension true 
+bundle exec htmlproofer ./_site --allow-hash-href true --assume-extension true --file-ignore ./_site/CHANGELOG.html
