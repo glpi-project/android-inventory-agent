3d3fc75ddca3ec0188ff1b3d05bf73e8102d04d9
Gianfranco Manganiello <gmanganiello@teclib.com>
2017-09-22 17:52:47 -0400

    docs(jekyll): fix html to reduce htmlproofer errors

--- ci/tets-jekyll.sh
+++ ci/tets-jekyll.sh
@@ -8,4 +8,5 @@
 set -e # halt script on error
 
 bundle exec jekyll build
-bundle exec htmlproofer ./_site
+rm -rf _site/reports
+bundle exec htmlproofer ./_site --allow-hash-href true --assume-extension true 
