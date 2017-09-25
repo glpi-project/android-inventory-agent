56f5e128f99a7413a860b804e0c5dacdfcddbd3b
Rafa Hernandez <rhernandez@teclib.com>
2017-09-25 15:54:46 -0400

    ci(coverage): change folder

--- ci/scripts/ci_coverage.sh
+++ ci/scripts/ci_coverage.sh
@@ -4,10 +4,10 @@
 ./gradlew createDebugCoverageReport
 
 # move code coverage
-mv -v inventory/build/reports/coverage reports$1
+mv -v app/build/reports/coverage reports$1
 
 #move Android test
-mv -v inventory/build/reports/androidTests reports$1
+mv -v app/build/reports/androidTests reports$1
 
 # replace .resources with resource because github don't support folders with "_" or "." at the beginning
 mv reports$1/debug/.resources reports$1/debug/resources
