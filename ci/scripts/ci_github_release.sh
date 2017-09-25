de8510cd2e0e111b96b7c91c2b5a94cd9f6b0099
Rafa Hernandez <rhernandez@teclib.com>
2017-09-25 18:35:59 -0400

    ci(release): upload zip

--- ci/scripts/ci_github_release.sh
+++ ci/scripts/ci_github_release.sh
@@ -4,6 +4,9 @@
 # conventional-github-releaser -p angular -t $GH_TOKEN -r 0
 GIT_TAG=$(jq -r ".version" package.json)
 
+# Create zip example code
+sudo zip -r $CIRCLE_ARTIFACTS/InventoryAgent-$GIT_TAG.zip app/build/outputs/apk/app-release.apk
+
 # Update release name
 github-release release \
 --user $CIRCLE_PROJECT_USERNAME \
@@ -11,3 +14,11 @@ github-release release \
 --tag ${GIT_TAG} \
 --name "Inventory Agent v${GIT_TAG}" \
 --description "$(git log -1 --pretty=%B)"
+
+# Upload example code release
+github-release upload \
+--user $CIRCLE_PROJECT_USERNAME \
+--repo $CIRCLE_PROJECT_REPONAME \
+--tag ${GIT_TAG} \
+--name "Inventory Agent zip" \
+--file $CIRCLE_ARTIFACTS/InventoryAgent-$GIT_TAG.zip
