bbcef2fd2d38a73aa926719b9f240fce9ec830f4
Rafa Hernandez <rhernandez@teclib.com>
2017-09-26 08:46:37 -0400

    ci(release): add apk file

--- ci/scripts/ci_github_release.sh
+++ ci/scripts/ci_github_release.sh
@@ -4,9 +4,6 @@
 # conventional-github-releaser -p angular -t $GH_TOKEN -r 0
 GIT_TAG=$(jq -r ".version" package.json)
 
-# Create zip example code
-sudo zip -r $CIRCLE_ARTIFACTS/InventoryAgent-$GIT_TAG.zip app/build/outputs/apk/app-release.apk
-
 # Update release name
 github-release release \
 --user $CIRCLE_PROJECT_USERNAME \
@@ -21,4 +18,4 @@ github-release upload \
 --repo $CIRCLE_PROJECT_REPONAME \
 --tag ${GIT_TAG} \
 --name "Inventory Agent zip" \
---file $CIRCLE_ARTIFACTS/InventoryAgent-$GIT_TAG.zip
+--file app/build/outputs/apk/app-release.apk
