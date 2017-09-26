b231662b2089babbdf4e061a5299f5cd4617fde4
Rafa Hernandez <rhernandez@teclib.com>
2017-09-26 11:27:56 -0400

    docs(remove): unused files

--- ci/tets-jekyll.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -e # halt script on error
-
-bundle exec jekyll build
-rm -rf _site/reports
-bundle exec htmlproofer ./_site --allow-hash-href true --assume-extension true --file-ignore ./_site/CHANGELOG.html
--- ci/yamllint.sh
+++
@@ -1,4 +0,0 @@
-./node_modules/.bin/yamllint _config.yml
-./node_modules/.bin/yamllint .eslintrc.yml
-./node_modules/.bin/yamllint .sass-lint.yml
-./node_modules/.bin/yamllint .travis.yml
