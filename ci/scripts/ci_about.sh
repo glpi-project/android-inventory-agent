2a2648d5f19352bb059ea61b4cbe4640a8b1fddd
Rafa Hernandez <rhernandez@teclib.com>
2017-09-28 16:02:21 -0400

    ci(about): remove delete file

--- ci/scripts/ci_about.sh
+++ ci/scripts/ci_about.sh
@@ -1,12 +1,8 @@
 #!/usr/bin/env bash
 
-if [[ -e app/src/main/assets/about.properties ]]; then
-    sudo rm app/src/main/assets/about.properties
-fi
-
 GIT_TAG=$(jq -r ".version" package.json)
 
-echo "about.version=${GIT_TAG}" >> app/src/main/assets/about.properties
+echo "about.version=${GIT_TAG}" > app/src/main/assets/about.properties
 echo "about.build=$CIRCLE_BUILD_NUM" >> app/src/main/assets/about.properties
 echo "about.date=$(date "+%a %b %d %H:%M:%S %Y")" >> app/src/main/assets/about.properties
 echo "about.commit=$(git rev-parse --verify --short=7 HEAD)" >> app/src/main/assets/about.properties
