284f9c0164dc8138607e9083d0cc54a5c1183c71
Rafa Hernandez <rhernandez@teclib.com>
2017-09-28 15:50:10 -0400

    ci(about): fix properties file name

--- ci/scripts/ci_about.sh
+++ ci/scripts/ci_about.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
-sudo rm app/src/main/assets/about.properties.md
+if [[ -e app/src/main/assets/about.properties ]]; then
+    sudo rm app/src/main/assets/about.properties
+fi
 
 GIT_TAG=$(jq -r ".version" package.json)
 
-echo "about.version=${GIT_TAG}" >> app/src/main/assets/about.properties.md
-echo "about.build=$CIRCLE_BUILD_NUM" >> app/src/main/assets/about.properties.md
-echo "about.date=$(date "+%a %b %d %H:%M:%S %Y")" >> app/src/main/assets/about.properties.md
-echo "about.commit=$(git rev-parse --verify --short=7 HEAD)" >> app/src/main/assets/about.properties.md
-echo "about.commitFull=$(git rev-parse --verify HEAD)" >> app/src/main/assets/about.properties.md
+echo "about.version=${GIT_TAG}" >> app/src/main/assets/about.properties
+echo "about.build=$CIRCLE_BUILD_NUM" >> app/src/main/assets/about.properties
+echo "about.date=$(date "+%a %b %d %H:%M:%S %Y")" >> app/src/main/assets/about.properties
+echo "about.commit=$(git rev-parse --verify --short=7 HEAD)" >> app/src/main/assets/about.properties
+echo "about.commitFull=$(git rev-parse --verify HEAD)" >> app/src/main/assets/about.properties
