3c59a2bd47a1480e5932d8631a0a90f177078b36
Rafa Hernandez <rhernandez@teclib.com>
2017-10-03 10:08:42 -0400

    ci(about): take the previous commit

--- ci/scripts/ci_about.sh
+++ ci/scripts/ci_about.sh
@@ -5,6 +5,6 @@ GIT_TAG=$(jq -r ".version" package.json)
 echo "about.version=${GIT_TAG}" > app/src/main/assets/about.properties
 echo "about.build=$CIRCLE_BUILD_NUM" >> app/src/main/assets/about.properties
 echo "about.date=$(date "+%a %b %d %H:%M:%S %Y")" >> app/src/main/assets/about.properties
-echo "about.commit=$(git rev-parse --verify --short=7 HEAD)" >> app/src/main/assets/about.properties
-echo "about.commitFull=$(git rev-parse --verify HEAD)" >> app/src/main/assets/about.properties
+echo "about.commit=$(git rev-parse --verify --short=7 HEAD~1)" >> app/src/main/assets/about.properties
+echo "about.commitFull=$(git rev-parse --verify HEAD~1)" >> app/src/main/assets/about.properties
 echo "about.github=https://github.com/flyve-mdm/flyve-mdm-android-inventory-agent" >> app/src/main/assets/about.properties
