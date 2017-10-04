41f2ed5cf89a372648d20c9445b944d1a36a5180
Rafa Hernandez <rhernandez@teclib.com>
2017-10-04 18:19:25 -0400

    ci(release): add conventional github releaser

--- ci/scripts/ci_github_release.sh
+++ ci/scripts/ci_github_release.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
 # push tag to github
-# conventional-github-releaser -p angular -t $GH_TOKEN -r 0
+conventional-github-releaser -p angular -t $GH_TOKEN -r 0
+
 GIT_TAG=$(jq -r ".version" package.json)
 
 # Update release name
-github-release release \
+github-release edit \
 --user $CIRCLE_PROJECT_USERNAME \
 --repo $CIRCLE_PROJECT_REPONAME \
 --tag ${GIT_TAG} \
 --name "Inventory Agent v${GIT_TAG}" \
---description "$(git log -1 --pretty=%B)"
 
 # Upload example code release
 github-release upload \
