4bcada6e69486b8058ed06c2d62033bce41004ad
Rafa Hernandez <rhernandez@teclib.com>
2017-10-05 09:33:56 -0400

    ci(merge): merge back the develop branch

--- ci/scripts/ci_push_changes.sh
+++ ci/scripts/ci_push_changes.sh
@@ -2,3 +2,14 @@
 
 # Push commits and tags to origin branch
 git push --follow-tags origin $CIRCLE_BRANCH
+
+# Merge back the develop branch step
+
+# go to develop
+git checkout develop
+
+# merge with master
+git merge master
+
+# return to master
+git checkout master
