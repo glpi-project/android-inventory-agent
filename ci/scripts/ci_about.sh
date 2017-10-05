2908d4018d38a70022de0cb22fde8d5697d5148c
Rafa Hernandez <rhernandez@teclib.com>
2017-10-05 08:42:37 -0400

    ci(about): add commit sliced

--- ci/scripts/ci_about.sh
+++ ci/scripts/ci_about.sh
@@ -5,6 +5,6 @@ GIT_TAG=$(jq -r ".version" package.json)
 echo "about.version=${GIT_TAG}" > app/src/main/assets/about.properties
 echo "about.build=$CIRCLE_BUILD_NUM" >> app/src/main/assets/about.properties
 echo "about.date=$(date "+%a %b %d %H:%M:%S %Y")" >> app/src/main/assets/about.properties
-echo "about.commit=$CIRCLE_SHA1" >> app/src/main/assets/about.properties
+echo "about.commit=${CIRCLE_SHA1:0:7}" >> app/src/main/assets/about.properties
 echo "about.commitFull=$CIRCLE_SHA1" >> app/src/main/assets/about.properties
 echo "about.github=https://github.com/flyve-mdm/flyve-mdm-android-inventory-agent" >> app/src/main/assets/about.properties
