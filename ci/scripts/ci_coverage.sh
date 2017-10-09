0d1b545fd927d6946278613ccf536c8880b42fc2
Rafa Hernandez <rhernandez@teclib.com>
2017-10-09 19:17:45 -0400

    ci(css): improve script to add css and headers

--- ci/scripts/ci_coverage.sh
+++ ci/scripts/ci_coverage.sh
@@ -1,25 +1,48 @@
-#!/usr/bin/env bash
+#!/bin/bash
+#
+#  Copyright (C) 2017 Teclib'
+#
+#  This file is part of Flyve MDM Inventory Android.
+#
+#  Flyve MDM Inventory Android is a subproject of Flyve MDM. Flyve MDM is a mobile
+#  device management software.
+#
+#  Flyve MDM Android is free software: you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License
+#  as published by the Free Software Foundation; either version 3
+#  of the License, or (at your option) any later version.
+#
+#  Flyve MDM Inventory Android is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  ------------------------------------------------------------------------------
+#  @author    Rafael Hernandez - rafaelje
+#  @copyright Copyright (c) 2017 Flyve MDM
+#  @license   GPLv3 https://www.gnu.org/licenses/gpl-3.0.html
+#  @link      https://github.com/flyve-mdm/flyve-mdm-android-inventory
+#  @link      http://www.glpi-project.org/
+#  @link      https://flyve-mdm.com/
+#  ------------------------------------------------------------------------------
+#
 
 # create code coverage report
 ./gradlew createDebugCoverageReport
 
 # move code coverage
-mv -v app/build/reports/coverage reports$1
+mv -v app/build/reports/coverage reports
 
 #move Android test
-mv -v app/build/reports/androidTests reports$1
+mv -v app/build/reports/androidTests reports
 
 # replace .resources with resource because github don't support folders with "_" or "." at the beginning
-mv reports$1/debug/.resources reports$1/debug/resources
+mv reports/debug/.resources reports/debug/resources
 
 # replace .sessions
-mv reports$1/debug/.sessions.html reports$1/debug/sessions.html
-
-# add header
-ruby ci/scripts/add_coverage_header.rb
+mv reports/debug/.sessions.html reports/debug/sessions.html
 
 # add code coverage and test result
-git add reports$1 -f
+git add reports -f
 
 # temporal commit
 git commit -m "tmp reports"
@@ -34,10 +57,25 @@ git checkout gh-pages
 sudo git clean -fdx
 
 # remove report folder
-sudo rm -R reports$1
+sudo rm -R reports
 
 # get documentation folder
-git checkout $CIRCLE_BRANCH reports$1
+git checkout $CIRCLE_BRANCH reports
+
+# remove css
+sudo rm ./reports/debug/resources/report.css
+sudo rm ./reports/androidTests/connected/css/base-style.css
+sudo rm ./reports/androidTests/connected/css/style.css
+
+# add new css
+cp ./css/coverage.css ./reports/debug/resources/report.css
+cp ./css/androidTests.css ./reports/androidTests/connected/css/style.css
+touch ./reports/androidTests/connected/css/base-style.css
+
+# add
+git add ./reports/debug/resources/report.css
+git add ./reports/androidTests/connected/css/style.css
+git add ./reports/androidTests/connected/css/base-style.css
 
 # create commit
 git commit -m "docs(coverage): update code coverage and test result"
--- ci/scripts/ci_javadoc.sh
+++ ci/scripts/ci_javadoc.sh
@@ -1,25 +1,41 @@
-#!/usr/bin/env bash
-
+#!/bin/bash
+#
+#  Copyright (C) 2017 Teclib'
+#
+#  This file is part of Flyve MDM Inventory Android.
+#
+#  Flyve MDM Inventory Android is a subproject of Flyve MDM. Flyve MDM is a mobile
+#  device management software.
+#
+#  Flyve MDM Android is free software: you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License
+#  as published by the Free Software Foundation; either version 3
+#  of the License, or (at your option) any later version.
+#
+#  Flyve MDM Inventory Android is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  ------------------------------------------------------------------------------
+#  @author    Rafael Hernandez - rafaelje
+#  @copyright Copyright (c) 2017 Flyve MDM
+#  @license   GPLv3 https://www.gnu.org/licenses/gpl-3.0.html
+#  @link      https://github.com/flyve-mdm/flyve-mdm-android-inventory
+#  @link      http://www.glpi-project.org/
+#  @link      https://flyve-mdm.com/
+#  ------------------------------------------------------------------------------
+#
 # Generate javadoc this folder must be on .gitignore
-javadoc -d ./reports$1/javadoc -sourcepath ./app/src/main/java -subpackages . -nonavbar
+javadoc -d ./reports/javadoc -sourcepath ./app/src/main/java -subpackages . -nonavbar
 
 # delete the index.html file
-sudo rm ./reports$1/javadoc/index.html
+sudo rm ./reports/javadoc/index.html
 
 # rename the overview-summary.html file toindex.html
-mv ./reports$1/javadoc/overview-summary.html ./reports$1/javadoc/index.html
-
-# add header
-ruby ci/scripts/add_javadoc_header.rb
-
-# remove default stylesheet.css
-sudo rm ./reports$1/javadoc/stylesheet.css
-
-# get stylesheet.css template
-wget -qO- https://gist.githubusercontent.com/flyve-mdm-bot/78014d4ffe3d5d70585a7b538f7eb84c/raw/fd12955bc582d968472a6d7f8b78ca5b8d4b8a23/stylesheet.css -O ./reports$1/javadoc/stylesheet.css
+mv ./reports/javadoc/overview-summary.html ./reports/javadoc/index.html
 
 # add reports
-git add reports$1 -f
+git add reports -f
 
 # create commit with temporary report folder
 git commit -m "tmp report commit"
@@ -31,16 +47,34 @@ git fetch origin gh-pages
 git checkout gh-pages
 
 # delete old javadoc folder
-sudo rm -R reports$1/javadoc
+sudo rm -R reports/javadoc
 
 # get fresh javadoc folder
-git checkout $CIRCLE_BRANCH reports$1/javadoc
+git checkout $CIRCLE_BRANCH reports/javadoc
+
+# remove default stylesheet.css
+sudo rm ./reports/javadoc/stylesheet.css
+
+# add new css
+cp ./css/javadoc.css ./reports/javadoc/stylesheet.css
 
 # git add javadoc folder
-git add reports$1/javadoc
+git add reports/javadoc
+
+# git add
+git add ./reports/javadoc/stylesheet.css
 
 # create commit for documentation
 git commit -m "docs(javadoc): update javadoc"
+
+# change headers
+ruby ci/add_header.rb
+
+# git add
+git add .
+
+# git commit
+git commit -m "docs(headers): update headers"
 
 # push to branch
 git push origin gh-pages
