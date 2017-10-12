da0a3a4b1f657562823bbea8a34190d5493085be
Gianfranco Manganiello <gmanganiello@teclib.com>
2017-10-12 15:35:34 -0400

    docs(sw): Create variable in the header for cache version

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
 ---
+cache_version: 00000000
 ---
-
 self.addEventListener('install', function(e) { 
   
   {% assign name = site.github.project_title | replace: "flyve-mdm-", "" %} 
-
-  var CACHE_NAME = '{{name}}-version-5'
+    
+  var CACHE_NAME = '{{ page.cache_version }}'
 
   caches.keys().then(function(cacheNames) {
     return Promise.all(
@@ -16,7 +16,7 @@ self.addEventListener('install', function(e) {
       })
     )
   })
-
+  
   e.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
       return cache.addAll([
@@ -26,20 +26,18 @@ self.addEventListener('install', function(e) {
         '{{ "/index.html" | absolute_url }}?homescreen=1',
         '{{ "/css/flyve-mdm.min.css" | absolute_url }}',
         '{{ "/css/main.css" | absolute_url }}',
-        '{{ "css/syntax.css" | absolute_url }}',
-        '{{ "images/lenovo.jpg" | absolute_url }}',
-        '{{ "images/xperia.jpg" | absolute_url }}',
-        '{{ "images/blackBerry.jpg" | absolute_url }}',
-        '{{ "images/typo.png" | absolute_url }}',
-        '{{ "images/logo.png" | absolute_url }}',
-        '{{ "js/app.js" | absolute_url }}',
-        '{{ "js/jquery.min.js" | absolute_url }}',
-        '{{ "js/bootstrap.min.js" | absolute_url }}',
-        '{{ "images/logo.png" | absolute_url }}',
-        '{{ "manifest.json" | absolute_url }}',
-        '{{ "fonts/glyphs/winjs-symbols.ttf" | absolute_url }}',
-        '{{ "images/logo.png" | absolute_url }}',
-        '{{ "fonts/selawk.ttf" | absolute_url }}',
+        '{{ "/css/syntax.css" | absolute_url }}',
+        '{{ "/images/typo.png" | absolute_url }}',
+        '{{ "/images/ipodTouch.png" | absolute_url }}',
+        '{{ "/images/ipad.png" | absolute_url }}',
+        '{{ "/images/IPhone6.png" | absolute_url }}',
+        '{{ "/images/logo.png" | absolute_url }}',
+        '{{ "/js/app.js" | absolute_url }}',
+        '{{ "/js/jquery.min.js" | absolute_url }}',
+        '{{ "/js/bootstrap.min.js" | absolute_url }}',
+        '{{ "/manifest.json" | absolute_url }}',
+        '{{ "/fonts/glyphs/winjs-symbols.ttf" | absolute_url }}',
+        '{{ "/fonts/selawk.ttf" | absolute_url }}',
       ])
     })
   )
