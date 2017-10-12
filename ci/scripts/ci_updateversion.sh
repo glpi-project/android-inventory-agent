f02a11ea4ccb0c2998da71c6bff0552b55b6d78d
Rafa Hernandez <rhernandez@teclib.com>
2017-10-12 14:23:16 -0400

    ci(git): remove local tags

--- ci/scripts/ci_updateversion.sh
+++ ci/scripts/ci_updateversion.sh
@@ -3,6 +3,9 @@
 # increment version code, need to be unique to send to store
 ./gradlew updateVersionCode -P vCode=$CIRCLE_BUILD_NUM
 
+# remove any local tag
+git tag | xargs git tag -d
+
 # increment version on package.json, create tag and commit with changelog
 npm run release -- -m "ci(release): generate CHANGELOG.md for version %s"
 
