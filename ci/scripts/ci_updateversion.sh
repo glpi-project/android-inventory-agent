cdc390602d668cdd07621d921e28d9fe3216a351
Rafa Hernandez <rhernandez@teclib.com>
2017-10-24 15:57:54 -0400

    ci(versioncode): update version code factor update

--- ci/scripts/ci_updateversion.sh
+++ ci/scripts/ci_updateversion.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # increment version code, need to be unique to send to store
-./gradlew updateVersionCode -P vCode=$(($CIRCLE_BUILD_NUM * 40))
+./gradlew updateVersionCode -P vCode=$(($CIRCLE_BUILD_NUM * 90))
 
 # remove any local tag
 git tag | xargs git tag -d
