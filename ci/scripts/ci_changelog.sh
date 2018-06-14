f0b41d8f42bd7cd7c389cf7a301af19b1d671da3
Naylin Medina <nmedina@teclib.com>
2018-06-14 09:45:16 -0400

    ci(circle): add commands for changelog
    Signed-off-by: Naylin Medina <nmedina@teclib.com>

--- ci/scripts/ci_changelog.sh
+++ ci/scripts/ci_changelog.sh
@@ -9,17 +9,23 @@ git checkout gh-pages
 # clean unstage file on gh-pages to remove all others files gets on checkout
 sudo git clean -fdx
 
-# remove local CHANGELOG.md on gh-pages
-sudo rm CHANGELOG.md
+# remove old CHANGELOG.md on gh-pages
+sudo rm _includes/CHANGELOG.md
 
 # get changelog from branch
 git checkout $CIRCLE_BRANCH CHANGELOG.md
 
+# move changelog to _includes folder for correct display
+sudo mv CHANGELOG.md _includes/CHANGELOG.md
+
 # add changelog
-git add CHANGELOG.md
+git add _includes/CHANGELOG.md && git add CHANGELOG.md
 
 # create a commit
 git commit -m "ci(changelog): update ChangeLog"
+
+# push to branch
+git push origin gh-pages
 
 # got back to original branch
 git checkout $CIRCLE_BRANCH
