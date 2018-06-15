c697dcccafab560385f43c5d5d33af93c1242adc
Naylin Medina <nmedina@teclib.com>
2018-06-15 14:51:16 -0400

    ci(circle): change command to update develop
    Signed-off-by: Naylin Medina <nmedina@teclib.com>

--- ci/scripts/ci_push_changes.sh
+++ ci/scripts/ci_push_changes.sh
@@ -43,8 +43,8 @@ git checkout develop
 # review some change
 git pull origin develop
 
-# merge with master
-git merge master
+# rebase with master
+git rebase master
 
 # push develop
 git push origin develop --force
