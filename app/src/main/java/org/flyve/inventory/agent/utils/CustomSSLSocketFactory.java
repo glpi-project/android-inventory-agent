eaff6e53ece62a2d8eac1b7a3c9b7ef63f726d53
Ivan Del Pino <idelpino@teclib.com>
2018-10-10 11:07:55 -0400

    fix(connection): validated https connection
    Signed-off-by: Ivan Del Pino <idelpino@teclib.com>

---
+++ app/src/main/java/org/flyve/inventory/agent/utils/CustomSSLSocketFactory.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright Teclib. All rights reserved.
+ *
+ * Flyve MDM is a mobile device management software.
+ *
+ * Flyve MDM is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * Flyve MDM is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * ------------------------------------------------------------------------------
+ * @author    Rafael Hernandez
+ * @author    Ivan Del Pino
+ * @copyright Copyright Teclib. All rights reserved.
+ * @license   GPLv3 https://www.gnu.org/licenses/gpl-3.0.html
+ * @link      https://github.com/flyve-mdm/android-inventory-agent
+ * @link      https://flyve-mdm.com
+ * ------------------------------------------------------------------------------
+ */
+
+package org.flyve.inventory.agent.utils;
+
+import org.apache.http.conn.ssl.SSLSocketFactory;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.security.KeyManagementException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.UnrecoverableKeyException;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+
+public class CustomSSLSocketFactory extends SSLSocketFactory {
+    SSLContext sslContext = SSLContext.getInstance("TLS");
+
+    public CustomSSLSocketFactory(KeyStore truststore) throws NoSuchAlgorithmException, KeyManagementException,
+            KeyStoreException, UnrecoverableKeyException {
+
+        super(truststore);
+        TrustManager tm = new CustomX509TrustManager();
+        sslContext.init(null, new TrustManager[] { tm }, null);
+    }
+
+    CustomSSLSocketFactory(SSLContext context)
+            throws KeyManagementException, NoSuchAlgorithmException,
+            KeyStoreException, UnrecoverableKeyException {
+        super(null);
+        sslContext = context;
+    }
+
+    @Override
+    public Socket createSocket(Socket socket, String host, int port,
+                               boolean autoClose) throws IOException, UnknownHostException {
+        return sslContext.getSocketFactory().createSocket(socket, host, port,
+                autoClose);
+    }
+
+    @Override
+    public Socket createSocket() throws IOException {
+        return sslContext.getSocketFactory().createSocket();
+    }
+}
---
+++ app/src/main/java/org/flyve/inventory/agent/utils/CustomX509TrustManager.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright Teclib. All rights reserved.
+ *
+ * Flyve MDM is a mobile device management software.
+ *
+ * Flyve MDM is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * Flyve MDM is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * ------------------------------------------------------------------------------
+ * @author    Rafael Hernandez
+ * @author    Ivan Del Pino
+ * @copyright Copyright Teclib. All rights reserved.
+ * @license   GPLv3 https://www.gnu.org/licenses/gpl-3.0.html
+ * @link      https://github.com/flyve-mdm/android-inventory-agent
+ * @link      https://flyve-mdm.com
+ * ------------------------------------------------------------------------------
+ */
+
+package org.flyve.inventory.agent.utils;
+
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.X509TrustManager;
+
+public class CustomX509TrustManager implements X509TrustManager {
+
+    @Override
+    public void checkClientTrusted(X509Certificate[] chain, String authType)
+            throws CertificateException {
+    }
+
+    @Override
+    public void checkServerTrusted(java.security.cert.X509Certificate[] certs,
+            String authType) throws CertificateException {
+
+        // Here you can verify the servers certificate. (e.g. against one which is stored on mobile device)
+
+        // InputStream inStream = null;
+        // try {
+        // inStream = MeaApplication.loadCertAsInputStream();
+        // CertificateFactory cf = CertificateFactory.getInstance("X.509");
+        // X509Certificate ca = (X509Certificate)
+        // cf.generateCertificate(inStream);
+        // inStream.close();
+        //
+        // for (X509Certificate cert : certs) {
+        // // Verifing by public key
+        // cert.verify(ca.getPublicKey());
+        // }
+        // } catch (Exception e) {
+        // throw new IllegalArgumentException("Untrusted Certificate!");
+        // } finally {
+        // try {
+        // inStream.close();
+        // } catch (IOException e) {
+        // e.printStackTrace();
+        // }
+        // }
+    }
+
+    public X509Certificate[] getAcceptedIssuers() {
+        return null;
+    }
+
+}
