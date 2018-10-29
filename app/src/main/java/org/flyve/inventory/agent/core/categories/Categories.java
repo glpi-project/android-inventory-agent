f358e6a005c01b0b6229f6d02133d83195d32f7f
Ivan Del Pino <idelpino@teclib.com>
2018-10-29 17:36:52 -0400

    feat(categories): add views and button
    Signed-off-by: Ivan Del Pino <idelpino@teclib.com>

---
+++ app/src/main/java/org/flyve/inventory/agent/core/categories/Categories.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright Teclib. All rights reserved.
+ *
+ * Flyve MDM is a mobile device management software.
+ *
+ * Flyve MDM is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * Flyve MDM is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * ------------------------------------------------------------------------------
+ * @author    Ivan Del Pino
+ * @copyright Copyright Teclib. All rights reserved.
+ * @license   GPLv3 https://www.gnu.org/licenses/gpl-3.0.html
+ * @link      https://github.com/flyve-mdm/android-mdm-agent
+ * @link      https://flyve-mdm.com
+ * ------------------------------------------------------------------------------
+ */
+
+package org.flyve.inventory.agent.core.categories;
+
+import android.content.Context;
+
+import org.flyve.inventory.agent.schema.ServerSchema;
+
+import java.util.ArrayList;
+
+public interface Categories {
+
+    interface View {
+        void showError(String message);
+        void successful(String message);
+        void modelServer(ServerSchema model);
+    }
+
+    interface Presenter {
+        // Views
+        void showError(String message);
+        void successful(String message);
+        void modelServer(ServerSchema model);
+
+        // Models
+        void saveServer(ArrayList<String> message, Context applicationContext);
+        void deleteServer(String serverName, Context applicationContext);
+        void updateServer(ArrayList<String> serverInfo, String serverName, Context applicationContext);
+        void loadServer(String serverName, Context applicationContext);
+    }
+
+    interface Model {
+        void saveServer(ArrayList<String> message, Context applicationContext);
+        void deleteServer(String serverName, Context applicationContext);
+        void updateServer(ArrayList<String> serverInfo, String serverName, Context applicationContext);
+        void loadServer(String serverName, Context applicationContext);
+    }
+}
---
+++ app/src/main/java/org/flyve/inventory/agent/core/categories/CategoriesPresenter.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright Teclib. All rights reserved.
+ *
+ * Flyve MDM is a mobile device management software.
+ *
+ * Flyve MDM is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * Flyve MDM is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * ------------------------------------------------------------------------------
+ * @author    Ivan Del Pino
+ * @copyright Copyright Teclib. All rights reserved.
+ * @license   GPLv3 https://www.gnu.org/licenses/gpl-3.0.html
+ * @link      https://github.com/flyve-mdm/android-mdm-agent
+ * @link      https://flyve-mdm.com
+ * ------------------------------------------------------------------------------
+ */
+
+package org.flyve.inventory.agent.core.categories;
+
+import android.content.Context;
+
+import org.flyve.inventory.agent.schema.ServerSchema;
+
+import java.util.ArrayList;
+
+public class CategoriesPresenter implements Categories.Presenter {
+
+    private Categories.View view;
+    private Categories.Model model;
+
+    public CategoriesPresenter(Categories.View view){
+        this.view = view;
+        model = new CategoriesModel(this);
+    }
+
+    @Override
+    public void showError(String message) {
+        if (view != null) {
+            view.showError(message);
+        }
+    }
+
+    @Override
+    public void successful(String message) {
+        if (view != null) {
+            view.successful(message);
+        }
+    }
+
+    @Override
+    public void modelServer(ServerSchema model) {
+        if (view != null) {
+            view.modelServer(model);
+        }
+    }
+
+    @Override
+    public void saveServer(ArrayList<String> message, Context applicationContext) {
+        model.saveServer(message, applicationContext);
+    }
+
+    @Override
+    public void deleteServer(String serverName, Context applicationContext) {
+        model.deleteServer(serverName, applicationContext);
+    }
+
+    @Override
+    public void updateServer(ArrayList<String> serverInfo, String serverName, Context applicationContext) {
+        model.updateServer(serverInfo, serverName, applicationContext);
+    }
+
+    @Override
+    public void loadServer(String serverName, Context applicationContext) {
+        model.loadServer(serverName, applicationContext);
+    }
+
+}
