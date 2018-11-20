603aa00204003494a3767f0b9985f8044bf74eac
Ivan Del Pino <idelpino@teclib.com>
2018-11-20 15:24:42 -0400

    bug(service): added permission foreground service
    Signed-off-by: Ivan Del Pino <idelpino@teclib.com>

--- app/src/main/java/org/flyve/inventory/agent/service/InventoryService.java
+++ app/src/main/java/org/flyve/inventory/agent/service/InventoryService.java
@@ -24,15 +24,21 @@
 package org.flyve.inventory.agent.service;
 
 import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Handler;
 import android.os.IBinder;
 import android.os.PowerManager;
 import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import org.flyve.inventory.InventoryTask;
@@ -74,7 +80,10 @@ public class InventoryService extends Service {
     @Override
     public void onCreate() {
         super.onCreate();
-        startForeground(1, new Notification());
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+            startMyOwnForeground();
+        else
+            startForeground(1, new Notification());
 
         mHandler = new Handler();
 
@@ -84,6 +93,27 @@ public class InventoryService extends Service {
         mTimer = new Timer();
         mTimer.scheduleAtFixedRate(new TimeDisplayTimerTask(), 5, NOTIFY_INTERVAL);
         intent = new Intent(TIMER_RECEIVER);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void startMyOwnForeground(){
+        String NOTIFICATION_CHANNEL_ID = "org.flyve.inventory.agent";
+        String channelName = "Inventory Service";
+        NotificationChannel chan = new NotificationChannel(NOTIFICATION_CHANNEL_ID, channelName, NotificationManager.IMPORTANCE_NONE);
+        chan.setLightColor(Color.BLUE);
+        chan.setLockscreenVisibility(Notification.VISIBILITY_PRIVATE);
+        NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        assert manager != null;
+        manager.createNotificationChannel(chan);
+
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID);
+        Notification notification = notificationBuilder.setOngoing(true)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle("App is running in background")
+                .setPriority(NotificationManager.IMPORTANCE_MIN)
+                .setCategory(Notification.CATEGORY_SERVICE)
+                .build();
+        startForeground(2, notification);
     }
 
     class TimeDisplayTimerTask extends TimerTask {
