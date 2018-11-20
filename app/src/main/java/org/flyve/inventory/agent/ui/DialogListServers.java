d1de75d8787acc12307fe6723e8f54f9ea25bc0f
Ivan Del Pino <idelpino@teclib.com>
2018-11-20 17:41:51 -0400

    fix(service): add parameter to inventory task in dialog list
    Signed-off-by: Ivan Del Pino <idelpino@teclib.com>

--- app/src/main/java/org/flyve/inventory/agent/ui/DialogListServers.java
+++ app/src/main/java/org/flyve/inventory/agent/ui/DialogListServers.java
@@ -113,7 +113,7 @@ public class DialogListServers {
         String message = activity.getResources().getString(R.string.loading);
         final ProgressDialog progressBar = ProgressDialog.show(activity, "Sending inventory", message);
 
-        final InventoryTask inventoryTask = new InventoryTask(activity, Helpers.getAgentDescription(activity));
+        final InventoryTask inventoryTask = new InventoryTask(activity, Helpers.getAgentDescription(activity), true);
         final HttpInventory httpInventory = new HttpInventory(activity);
         final ServerSchema model = httpInventory.setServerModel(server);
         inventoryTask.setTag(model.getTag());
