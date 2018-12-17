eaa2d16e18f138210310e5258be8a734babf59f5
Naylin Medina <nmedina@teclib.com>
2018-12-17 12:05:53 -0400

    docs(headers): remove author tag
    Signed-off-by: Naylin Medina <nmedina@teclib.com>

--- ci/scripts/github_release.sh
+++ ci/scripts/github_release.sh
@@ -2,29 +2,27 @@
 #
 #  LICENSE
 #
-#  This file is part of Flyve MDM Agent for Android.
+#  This file is part of Flyve MDM Inventory Agent for Android.
 #
-#  Flyve MDM Agent for Android is a subproject of Flyve MDM. Flyve MDM is a mobile
-#  device management software.
+#  Inevntory Agent for Android is a subproject of Flyve MDM. 
+#  Flyve MDM is a mobile device management software.
 #
 #  Flyve MDM is free software: you can redistribute it and/or
 #  modify it under the terms of the GNU General Public License
 #  as published by the Free Software Foundation; either version 3
 #  of the License, or (at your option) any later version.
 #
-#  Flyve MDM Agent for Android is distributed in the hope that it will be useful,
+#  Flyve MDM is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
-#  --------------------------------------------------------------------------------
-#  @author    Rafael Hernandez - <rhernandez@teclib.com>
-#  @author    Naylin Medina    - <nmedina@teclib.com>
-#  @copyright Copyright (c) Teclib'
+#  ---------------------------------------------------------------------
+#  @copyright Copyright © 2018 Teclib. All rights reserved.
 #  @license   GPLv3 https://www.gnu.org/licenses/gpl-3.0.html
-#  @link      https://github.com/flyve-mdm/android-mdm-agent/
-#  @link      http://flyve.org/android-mdm-agent/
+#  @link      https://github.com/flyve-mdm/android-inventory-agent/
+#  @link      http://flyve.org/android-inventory-agent/
 #  @link      https://flyve-mdm.com/
-#  --------------------------------------------------------------------------------
+#  ---------------------------------------------------------------------
 #
 
 # push tag to github
