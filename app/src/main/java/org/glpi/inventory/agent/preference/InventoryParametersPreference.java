3e53cc4b4a2c79521df09c6320eac4fbc5dbc6e4
Teclib <skita@teclib.com>
2019-11-28 15:21:00 +0100

    fix(design): change UI/UX
    Signed-off-by: Teclib <skita@teclib.com>

--- app/src/main/java/org/glpi/inventory/agent/preference/InventoryParametersPreference.java
+++ app/src/main/java/org/glpi/inventory/agent/preference/InventoryParametersPreference.java
@@ -41,6 +41,10 @@ import android.os.Bundle;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceManager;
 import android.support.v4.content.LocalBroadcastManager;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
 
 import org.glpi.inventory.agent.R;
 
@@ -52,6 +56,17 @@ public class InventoryParametersPreference extends PreferenceActivity
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.inventory_parameters);
         PreferenceManager.getDefaultSharedPreferences(this).registerOnSharedPreferenceChangeListener(this);
+        LinearLayout root = (LinearLayout)findViewById(android.R.id.list).getParent().getParent().getParent();
+        Toolbar bar = (Toolbar) LayoutInflater.from(this).inflate(R.layout.toolbar, root, false);
+        bar.setTitle(R.string.AccueilInventoryParamSummary);
+        root.addView(bar, 0); // insert at top
+        bar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
     }
 
     @Override
