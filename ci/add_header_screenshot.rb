3cd7824ad4c0ea5fa392102d887e020f54e9e33b
Teclib <skita@teclib.com>
2019-12-10 14:28:50 +0100

    fix(gh-pages): update style / licence
    Signed-off-by: Teclib <skita@teclib.com>

--- ci/add_header_screenshot.rb
+++ ci/add_header_screenshot.rb
@@ -1,28 +1,36 @@
 #
-#  Copyright (C) 2017 Teclib'
+# ---------------------------------------------------------------------
+# GLPI Android Inventory Agent
+# Copyright (C) 2019 Teclib.
 #
-#  This file is part of Flyve MDM Inventory Android.
+# https://glpi-project.org
 #
-#  Flyve MDM Inventory Android is a subproject of Flyve MDM. Flyve MDM is a mobile
-#  device management software.
+# Based on Flyve MDM Inventory Agent For Android
+# Copyright © 2018 Teclib. All rights reserved.
 #
-#  Flyve MDM Android is free software: you can redistribute it and/or
+# ---------------------------------------------------------------------
+#
+#  LICENSE
+#
+#  This file is part of GLPI Android Inventory Agent.
+#
+#  GLPI Android Inventory Agent is a subproject of GLPI.
+#
+#  GLPI Android Inventory Agent is free software: you can redistribute it and/or
 #  modify it under the terms of the GNU General Public License
 #  as published by the Free Software Foundation; either version 3
 #  of the License, or (at your option) any later version.
 #
-#  Flyve MDM Inventory Android is distributed in the hope that it will be useful,
+#  GLPI Android Inventory Agent is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
-#  ------------------------------------------------------------------------------
-#  @author    Rafael Hernandez - rafaelje
-#  @copyright Copyright (c) 2017 Flyve MDM
+#  ---------------------------------------------------------------------
+#  @copyright Copyright © 2019 Teclib. All rights reserved.
 #  @license   GPLv3 https://www.gnu.org/licenses/gpl-3.0.html
-#  @link      https://github.com/flyve-mdm/android-inventory-agent
-#  @link      http://www.glpi-project.org/
-#  @link      https://flyve-mdm.com/
-#  ------------------------------------------------------------------------------
+#  @link      https://github.com/glpi-project/android-inventory-agent
+#  @link      https://glpi-project.org/glpi-network/
+#  ---------------------------------------------------------------------
 #
 
 # add screenshots header
