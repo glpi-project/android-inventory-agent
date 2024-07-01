13592f73afc07eb2311df03e9b09faaeb52bac44
stonebuzz <skita@teclib.com>
2024-07-01 14:11:44 +0200

    Feat(Core): handle EMM managed configurations

--- app/src/main/java/org/glpi/inventory/agent/ui/ActivityMain.java
+++ app/src/main/java/org/glpi/inventory/agent/ui/ActivityMain.java
@@ -40,11 +40,13 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.RestrictionsManager;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
+import android.os.Parcelable;
 import android.preference.PreferenceManager;
 
 import androidx.appcompat.app.ActionBarDrawerToggle;
@@ -52,6 +54,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.app.ActivityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.enterprise.feedback.KeyedAppState;
+import androidx.enterprise.feedback.KeyedAppStatesReporter;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.localbroadcastmanager.content.LocalBroadcastManager;
@@ -66,18 +70,27 @@ import android.widget.TextView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import org.flyve.inventory.InventoryTask;
 import org.glpi.inventory.agent.R;
+import org.glpi.inventory.agent.core.detailserver.DetailServer;
+import org.glpi.inventory.agent.core.detailserver.DetailServerPresenter;
 import org.glpi.inventory.agent.core.main.Main;
 import org.glpi.inventory.agent.core.main.MainPresenter;
 import org.glpi.inventory.agent.preference.GlobalParametersPreference;
 import org.glpi.inventory.agent.preference.InventoryParametersPreference;
+import org.glpi.inventory.agent.schema.ServerSchema;
 import org.glpi.inventory.agent.service.InventoryService;
+import org.glpi.inventory.agent.utils.AgentLog;
 import org.glpi.inventory.agent.utils.Helpers;
+import org.glpi.inventory.agent.utils.HttpInventory;
 import org.glpi.inventory.agent.utils.LocalPreferences;
 import org.glpi.inventory.agent.utils.LocalStorage;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 
 public class ActivityMain extends AppCompatActivity
@@ -115,11 +128,150 @@ public class ActivityMain extends AppCompatActivity
         }
     };
 
+    private BroadcastReceiver appRestrictionChange = null;
+    private KeyedAppStatesReporter appRestrictionChangeReporter = null;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        IntentFilter restrictionsFilter =
+                new IntentFilter(Intent.ACTION_APPLICATION_RESTRICTIONS_CHANGED);
+
+        BroadcastReceiver appRestrictionChange = new BroadcastReceiver() {
+            @Override public void onReceive(Context context, Intent intent) {
+                resolveRestrictions();
+            }
+        };
+
+        registerReceiver(appRestrictionChange, restrictionsFilter);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (appRestrictionChange != null) {
+            unregisterReceiver(appRestrictionChange);
+            appRestrictionChange = null;
+        }
+    }
+
+    public static void enterpriseFeedback(Context context,
+                                          String key,
+                                          String message,
+                                          String data,
+                                          int severity) {
+        KeyedAppStatesReporter keyedAppStatesReporter = KeyedAppStatesReporter.create(context);
+        KeyedAppState keyedAppStateMessage = KeyedAppState.builder()
+                .setSeverity(severity)
+                .setKey(key)
+                .setMessage(message)
+                .setData(data)
+                .build();
+        List<KeyedAppState> list = new ArrayList<>();
+        list.add(keyedAppStateMessage);
+        keyedAppStatesReporter.setStates(list);
+    }
+
+    private void resolveRestrictions() {
+        AgentLog.e("EMM - START resolve restrictions");
+        RestrictionsManager myRestrictionsMgr = null;
+        myRestrictionsMgr = (RestrictionsManager) getSystemService(Context.RESTRICTIONS_SERVICE);
+        Bundle appRestrictions = myRestrictionsMgr.getApplicationRestrictions();
+
+        SharedPreferences customSharedPreference = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        SharedPreferences.Editor editor = customSharedPreference.edit();
+
+        if (appRestrictions.containsKey("automatic_inventory")) {
+            editor.putBoolean("autoStartInventory", appRestrictions.getBoolean("automatic_inventory"));
+            enterpriseFeedback(getApplicationContext(), "automatic_inventory", "automatic_inventory option set successfully", appRestrictions.getBoolean("automatic_inventory") ? "true" : "false", KeyedAppState.SEVERITY_INFO);
+            AgentLog.e("EMM - set automatic inventory to " + appRestrictions.getBoolean("automatic_inventory"));
+            editor.apply();
+        }
+
+        if (appRestrictions.containsKey("frequency")) {
+            editor.putString("timeInventory", appRestrictions.getString("frequency"));
+            enterpriseFeedback(getApplicationContext(), "frequency", "frequency option set successfully", appRestrictions.getString("frequency"), KeyedAppState.SEVERITY_INFO);
+            AgentLog.e("EMM - set frequency to " + appRestrictions.getString("frequency"));
+            editor.apply();
+        }
+
+        if (appRestrictions.containsKey("auto_start_on_boot")) {
+            editor.putBoolean("boot", appRestrictions.getBoolean("auto_start_on_boot"));
+            enterpriseFeedback(getApplicationContext(), "auto_start_on_boot", "auto_start_on_boot option set successfully", appRestrictions.getBoolean("auto_start_on_boot") ? "true" : "false", KeyedAppState.SEVERITY_INFO);
+            AgentLog.e("EMM - set auto start on boot to " + appRestrictions.getBoolean("auto_start_on_boot"));
+            editor.apply();
+        }
+
+        Parcelable[] parcelables = appRestrictions.getParcelableArray("server_configuration_list");
+        if (parcelables != null && parcelables.length > 0) {
+            final Context context = getApplicationContext();
+            for (int i = 0; i < parcelables.length; i++) {
+                Bundle serverConfig = (Bundle) parcelables[i];
+                JSONObject jsonServerConfig = new JSONObject();
+                LocalPreferences preferences = new LocalPreferences(context);
+
+                if (serverConfig.getString("server_url").isEmpty()) {
+                    enterpriseFeedback(getApplicationContext(), "server_url", "Error server URL is mandatory -> ", serverConfig.getString("server_url"), KeyedAppState.SEVERITY_ERROR);
+                    AgentLog.e("EMM - server url is mandatory");
+                    continue;
+                }
+
+                try {
+                    jsonServerConfig.put("address", serverConfig.getString("server_url"));
+                    jsonServerConfig.put("tag", serverConfig.getString("server_tag"));
+                    jsonServerConfig.put("login", serverConfig.getString("server_login"));
+                    jsonServerConfig.put("pass", serverConfig.getString("server_password"));
+                    jsonServerConfig.put("itemtype", serverConfig.getString("server_itemtype"));
+                    jsonServerConfig.put("serial", serverConfig.getString("server_custom_asset_serial"));
+
+                    AgentLog.e("EMM - Receive the following configuration '" + jsonServerConfig.toString());
+
+                    JSONObject local_server = preferences.loadJSONObject(serverConfig.getString("server_url"));
+                    AgentLog.e("EMM - Try to load '" + serverConfig.getString("server_url") + "' server if exist");
+                    AgentLog.e("EMM - Found '" + local_server.toString() + "'");
+                    AgentLog.e("EMM - Exist ? -> '" + !local_server.toString().equals("{}") + "'");
+
+                    if (local_server.toString().equals("{}")) {
+                        ArrayList<String> serverArray = preferences.loadServer();
+                        serverArray.add(serverConfig.getString("server_url"));
+                        preferences.saveServer(serverArray);
+                        preferences.saveJSONObject(serverConfig.getString("server_url"), jsonServerConfig);
+                        enterpriseFeedback(getApplicationContext(), "server_url", "server_url added successfully", serverConfig.getString("server_url"), KeyedAppState.SEVERITY_INFO);
+                        enterpriseFeedback(getApplicationContext(), "server_tag", "server_tag added successfully", serverConfig.getString("server_tag"), KeyedAppState.SEVERITY_INFO);
+                        enterpriseFeedback(getApplicationContext(), "server_login", "server_login added successfully", serverConfig.getString("server_login"), KeyedAppState.SEVERITY_INFO);
+                        enterpriseFeedback(getApplicationContext(), "server_password", "server_password added successfully", "***", KeyedAppState.SEVERITY_INFO);
+                        enterpriseFeedback(getApplicationContext(), "server_itemtype", "server_itemtype added successfully", serverConfig.getString("server_itemtype"), KeyedAppState.SEVERITY_INFO);
+                        enterpriseFeedback(getApplicationContext(), "server_custom_asset_serial", "server_custom_asset_serial added successfully", serverConfig.getString("server_custom_asset_serial"), KeyedAppState.SEVERITY_INFO);
+                        AgentLog.e("EMM - Server added successfully");
+                    } else {
+                        preferences.deletePreferences(serverConfig.getString("server_url"));
+                        preferences.saveJSONObject(serverConfig.getString("server_url"), jsonServerConfig);
+                        enterpriseFeedback(getApplicationContext(), "server_url", "server_url updated successfully", serverConfig.getString("server_url"), KeyedAppState.SEVERITY_INFO);
+                        enterpriseFeedback(getApplicationContext(), "server_tag", "server_tag updated successfully", serverConfig.getString("server_tag"), KeyedAppState.SEVERITY_INFO);
+                        enterpriseFeedback(getApplicationContext(), "server_login", "server_login updated successfully", serverConfig.getString("server_login"), KeyedAppState.SEVERITY_INFO);
+                        enterpriseFeedback(getApplicationContext(), "server_password", "server_password updated successfully", "***", KeyedAppState.SEVERITY_INFO);
+                        enterpriseFeedback(getApplicationContext(), "server_itemtype", "server_itemtype updated successfully", serverConfig.getString("server_itemtype"), KeyedAppState.SEVERITY_INFO);
+                        enterpriseFeedback(getApplicationContext(), "server_custom_asset_serial", "server_custom_asset_serial updated successfully", serverConfig.getString("server_custom_asset_serial"), KeyedAppState.SEVERITY_INFO);
+                        AgentLog.e("EMM - Server updated successfully");
+                    }
+
+                } catch (JSONException e) {
+                    enterpriseFeedback(getApplicationContext(), "server_url", "error while adding/updating server -> " + e.getMessage(), serverConfig.getString("server_url"), KeyedAppState.SEVERITY_ERROR);
+                    AgentLog.e("EMM - error while adding/updating server");
+                    AgentLog.e("EMM - " + e.getMessage());
+                }
+            }
+        } else {
+            AgentLog.e("EMM - 'server_configuration_list' key is empty");
+        }
+        AgentLog.e("EMM - END resolve restrictions");
+    }
 
     @Override
     protected void onResume() {
         super.onResume();
         registerReceiver(broadcastReceiver,new IntentFilter(InventoryService.TIMER_RECEIVER));
+        resolveRestrictions();
     }
 
     @Override
@@ -234,6 +386,10 @@ public class ActivityMain extends AppCompatActivity
                 ActivityMain.this.startActivity(miIntent);
             }
         });
+
+        //app restriction change
+        KeyedAppStatesReporter appRestrictionChangeReporter = KeyedAppStatesReporter.create(getApplicationContext());
+
 
     }
 
